8c48858ab0d5d91a1120c16ebd7b6aac8b45ec55
muhsinahm <muhsingcu@gmail.com>
2025-11-03 01:34:06 +0500

    Exploring HTTP calls

--- src/app.js
+++ src/app.js
@@ -3,15 +3,36 @@ const express = require("express");
 const app = express();
 
 //request handlers
-app.get("/", (req, res) => {
-  res.send("Homing from the server!");
+//order of code matters here
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello   from the server!");
+// });
+
+app.use("/user", (req, res) => {
+  res.send("Updated Homing from the server!");
 });
-app.get("/hello", (req, res) => {
-  res.send("Hello   from the server!");
+
+//this will handle only GET calls to /test
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Muhsin", lastName: "Ali" });
 });
-app.get("/test", (req, res) => {
+
+app.post("/user", (req, res) => {
+  // saving data to DB
+  // console.log("Save Data to the database");
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
+});
+
+//this will macth all HTTP method API calls to /test
+app.use("/test", (req, res) => {
   res.send("Testing from the server!");
 });
+
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
 });
